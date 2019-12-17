3cfcaddfc93284b51dc887447820bcf43a986946
YCullen <ycullen@163.com>
2019-12-17 22:10:36 +0800

    11

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -3,7 +3,6 @@ import ipdb
 def twoSum(nums,target):
     for i in range(0,len(nums)):
         j = len(nums)-1
-        ipdb.set_trace()
         while i<j:
             if nums[i] == target - nums[j]:
                 ans = [i,j]
@@ -14,7 +13,6 @@ def twoSum(nums,target):
 
 if __name__=='__main__':
     lst = [3,2,4]
-    ipdb.set_trace()
     ans = twoSum(lst,6)
     ans = twoSum(lst,7)
     print(ans)
